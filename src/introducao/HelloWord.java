a99e73cb29080db847f8c320f871d41bd7b58f45
Thiago Lima <thiagolima6623@gmail.com>
2026-04-17 10:23:20 -0300

    Inicialização do projeto

---
+++ src/introducao/HelloWord.java
@@ -0,0 +1,13 @@
+package introducao;
+
+public class HelloWord {
+
+	public static void main(String[] args) {	// comentario - metodo principal que executa o código
+		
+		// Nossa logica aqui
+		
+		System.out.print("Hello World");  // identico ao escreva () do Portugol
+
+	}
+
+}
