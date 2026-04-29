f60f78c9b9b671e0ecbc5621fa4ff1d584482733
Thiago Lima <thiagolima6623@gmail.com>
2026-04-29 12:13:59 -0300

    Adicinando exemplo de poo

---
+++ src/poo/TestaCarro.java
@@ -0,0 +1,28 @@
+package poo;
+
+public class TestaCarro {
+
+	public static void main(String[] args) {
+
+		// Instanciamento | Instanciar um objeto
+		Carro c1 = new Carro("Kombi", 4, 4, "Modelo "); // Buscando a planta
+        Carro c2 = new Carro("Toyota", 2, 4, "Supra");                                              // c1 é um objeto
+		// c1.marca = "Kombi";
+		// c1.portas = 4;
+
+		/*
+	    //c1.setMarca("Kombi"); 
+	    //c1.setPortas(4); 
+	    //c1.locomover();
+		 */
+
+		System.out.println("Marca: " + c1.getMarca());
+		System.out.println("Qtd. Portas: " + c1.getPortas());
+        System.out.println(c1.parar());
+        
+        System.out.println("\nMarca: " + c2.getMarca());
+		System.out.println("Qtd. Portas: " + c2.getPortas());
+		System.out.println(c2.parar());
+	}
+
+}
